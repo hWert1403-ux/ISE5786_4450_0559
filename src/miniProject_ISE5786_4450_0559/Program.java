742b70ee74ea390612b5c0608a76781d5039c46f
hadas wert <hadaswert1403@gmail.com>
2026-03-10 16:03:54 +0200

    change to hello

--- src/miniProject_ISE5786_4450_0559/Program.java
+++ src/miniProject_ISE5786_4450_0559/Program.java
@@ -3,7 +3,7 @@ package miniProject_ISE5786_4450_0559;
 public class Program {
 
 	public static void main(String[] args) {
-    	System.out.println("---");
+    	System.out.println("--hello-");
 
 	}
 }
