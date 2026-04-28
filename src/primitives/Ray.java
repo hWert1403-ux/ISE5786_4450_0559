de158f4e2f4cf34504d4a9b0b04e09c48c9ae421
hadas wert <hadaswert1403@gmail.com>
2026-04-28 20:47:01 +0300

    Add: function testFindClosestPoint at Ray class

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -83,27 +83,26 @@ public final class Ray {
 			return _origin;
 		}
 	}
-
+	
 	/**
 	 * find the colosest point of the intersection points list
-	 * 
 	 * @param points - intersection points list
 	 * @return closest point to origin, or null if the list is null
 	 */
 	public Point findClosestPoint(List<Point> points) {
-		if (points == null)
+		if(points == null)
 			return null;
-
+		
 		Point closest = null;
-		double minDistance = Double.POSITIVE_INFINITY;
+	    double minDistance = Double.POSITIVE_INFINITY;
 
-		for (Point p : points) {
-			double distance = _origin.distanceSquared(p); // Use squared distance for efficiency
-			if (distance < minDistance) {
-				minDistance = distance;
-				closest = p;
-			}
-		}
-		return closest;
+	    for (Point p : points) {
+	        double distance = _origin.distanceSquared(p); // Use squared distance for efficiency 
+	        if (distance < minDistance) {
+	            minDistance = distance;
+	            closest = p;
+	        }
+	    }
+	    return closest;
 	}
 }
